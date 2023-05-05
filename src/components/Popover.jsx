bf40c238fd35aa721cda742e1c84197f440a6453
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-06 03:02:11 +0330

    :bug: fix: change popover panel style classes

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -30,7 +30,7 @@ const solutions = [
 
 const PopoverLink = () => {
 	return (
-		<div className="fixed w-full max-w-sm px-4 pt-3">
+		<div className="w-full max-w-sm px-4 pt-3">
 			<Popover className="relative">
 				{({ open }) => (
 					<>
@@ -56,7 +56,7 @@ const PopoverLink = () => {
 							leaveFrom="opacity-100 translate-y-0"
 							leaveTo="opacity-0 translate-y-1"
 						>
-							<Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+							<Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/4 transform px-4 sm:px-0 lg:max-w-3xl">
 								<div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
 									<div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
 										{solutions.map((item) => (
