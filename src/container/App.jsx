eb5bb74f72cbc78f04a2886ccc6ae67c78a162a2
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-07 03:10:26 +0330

    :art: refactor: change index css to app css, move app files to container and add chakra provider on app file

---
+++ src/container/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const App = () => {
+	return (
+		//* Wrap ChakraProvider at the root of your app
+		<ChakraProvider>
+			{/* <TheRestOfYourApplication /> */}
+			{/* <TheRestOfYourApplication /> */}
+		</ChakraProvider>
+	);
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import App from "./App.jsx";
-import "./index.css";
+import App from "./container/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
